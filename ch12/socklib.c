7205eec49222c48b475345a830e8b716e87cafc1
haohong725 <haohong725@qq.com>
2018-04-24 22:20:58 +0800

    server demo

--- ch12/socklib.c
+++ ch12/socklib.c
@@ -1,72 +1,92 @@
-#include<stdio.h>
-#include<unistd.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<netinet/in.h>
-#include<netdb.h>
-#include<time.h>
-#include<strings.h>
-#include<stdlib.h>
+/*
+ *	socklib.c
+ *
+ *	This file contains functions used lots when writing internet
+ *	client/server programs.  The two main functions here are:
+ *
+ *	make_server_socket( portnum )	returns a server socket
+ *					or -1 if error
+ *      make_server_socket_q(portnum,backlog)
+ *
+ *	connect_to_server(char *hostname, int portnum)
+ *					returns a connected socket
+ *					or -1 if error
+ */ 
 
-#define HOSTLEN 256
-#define BACKLOG 1
+#include	<stdio.h>
+#include	<unistd.h>
+#include	<sys/types.h>
+#include	<sys/socket.h>
+#include	<netinet/in.h>
+#include	<netdb.h>
+#include	<time.h>
+#include	<strings.h>
 
-int make_server_socket_q(int, int);
+#define   HOSTLEN  256
+#define	  BACKLOG  1
 
-int make_server_socket(int protnum)
+int make_server_socket_q(int , int );
+
+int make_server_socket(int portnum)
 {
-	return make_server_socket_q(protnum, BACKLOG);
+	return make_server_socket_q(portnum, BACKLOG);
 }
-
 int make_server_socket_q(int portnum, int backlog)
 {
-	struct sockaddr_in saddr;
-	struct hostent * hp;
-	char hostname[HOSTLEN];
-	int sock_id;
+	struct  sockaddr_in   saddr;   /* build our address here */
+	struct	hostent		*hp;   /* this is part of our    */
+//	char	hostname[HOSTLEN];     /* address 	         */
+	char	hostname[] = "localhost";     /* address 	         */
+	int	sock_id;	       /* the socket             */
 
-	sock_id = socket(PF_INET, SOCK_STREAM, 0);
-	if (sock_id == -1)
+	sock_id = socket(PF_INET, SOCK_STREAM, 0);  /* get a socket */
+	if ( sock_id == -1 ) 
 		return -1;
 
-	bzero((void *)&saddr, sizeof(saddr));
-	gethostname(hostname, HOSTLEN);
-	hp = gethostbyname(hostname);
+	/** build address and bind it to socket **/
 
-	bcopy((void *)hp->h_addr, (void *)&saddr.sin_addr, hp->h_length);
-	saddr.sin_port = htons(portnum);
-	saddr.sin_family = AF_INET;
+	bzero((void *)&saddr, sizeof(saddr));   /* clear out struct     */
+//	gethostname(hostname, HOSTLEN);         /* where am I ?         */
 
-	if (bind(sock_id, (struct sockaddr *)&saddr, sizeof(saddr)) != 0)
+	hp = gethostbyname(hostname);           /* get info about host  */
+	                                        /* fill in host part    */
+	bcopy( (void *)hp->h_addr, (void *)&saddr.sin_addr, hp->h_length);
+	saddr.sin_port = htons(portnum);        /* fill in socket port  */
+	saddr.sin_family = AF_INET ;            /* fill in addr family  */
+	if ( bind(sock_id, (struct sockaddr *)&saddr, sizeof(saddr)) != 0 )
+	       return -1;
+
+	/** arrange for incoming calls **/
+
+	if ( listen(sock_id, backlog) != 0 ) 
 		return -1;
-
-	if (listen(sock_id, backlog) != 0)
-		return -1;
-
 	return sock_id;
 }
 
-int connect_to_server(char * host, int portnum)
+int connect_to_server(char *host, int portnum)
 {
 	int sock;
-	struct sockaddr_in servadd;
-	struct hostent *hp;
+	struct sockaddr_in  servadd;        /* the number to call */
+	struct hostent      *hp;            /* used to get number */
 
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-	if (sock == -1)
+	/** Step 1: Get a socket **/
+
+	sock = socket( AF_INET, SOCK_STREAM, 0 );    /* get a line   */
+	if ( sock == -1 ) 
 		return -1;
 
-	bzero(&servadd, sizeof(servadd));
-	hp = gethostbyname(host);
-	if (hp == NULL)
-		return -1;
+	/** Step 2: connect to server **/
 
+	bzero( &servadd, sizeof(servadd) );     /* zero the address     */
+	hp = gethostbyname( host );             /* lookup host's ip #   */
+	if (hp == NULL) 
+		return -1;
 	bcopy(hp->h_addr, (struct sockaddr *)&servadd.sin_addr, hp->h_length);
-	servadd.sin_port = htons(portnum);
-	servadd.sin_family = AF_INET;
+	servadd.sin_port = htons(portnum);      /* fill in port number  */
+	servadd.sin_family = AF_INET ;          /* fill in socket type  */
 
-	if (connect(sock, (struct sockaddr *)&servadd, sizeof(servadd)) != 0)
-		return -1;
+	if ( connect(sock,(struct sockaddr *)&servadd, sizeof(servadd)) !=0)
+	       return -1;
 
 	return sock;
 }
